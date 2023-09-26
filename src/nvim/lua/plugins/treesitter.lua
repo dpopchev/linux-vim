d4553c34dc42477d8f28362a3939f58bff585bb7
Dimitar Popchev <dpopchev@gmail.com>
2023-09-26 13:23:14 +0300

    add treesitter

---
+++ src/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,44 @@
+local TREESITTER_CONFIGS = {
+    highlight = { enable = true },
+    indent = { enable = true },
+    ensure_installed = {
+        -- commenting out as auto_install does not stop it for checking
+        -- "bash",
+        -- "json",
+        -- "lua",
+        -- "luadoc",
+        -- "markdown",
+        -- "markdown_inline",
+        -- "python",
+        -- "query",
+        -- "regex",
+        -- "vim",
+        -- "vimdoc",
+        -- "yaml",
+    },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "gnn",
+            node_incremental = "gn]",
+            node_decremental = "gn[",
+            scope_incremental = false,
+        },
+    },
+    auto_install = false,
+    sync_install = false,
+}
+
+local function make_treesitter_config()
+    require('nvim-treesitter.configs').setup(TREESITTER_CONFIGS)
+end
+
+local TREESITTER = {
+    "nvim-treesitter/nvim-treesitter",
+    config = make_treesitter_config
+}
+
+return {
+    {'haringsrob/nvim_context_vt', dependencies = {TREESITTER} },
+    {'nvim-treesitter/nvim-treesitter-context', dependencies = {TREESITTER} },
+}
