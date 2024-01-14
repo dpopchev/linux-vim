6de1b578aa97a6ad24aad9623e5b71084fc96d07
Dimitar Popchev <dpopchev@gmail.com>
2024-01-14 14:38:54 +0200

    chage telescope mapping for git_fiels as C-p raised excettio; see issue #2866

--- src/nvim/lua/plugins/telescope.lua
+++ src/nvim/lua/plugins/telescope.lua
@@ -1,9 +1,9 @@
 local function setup()
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ef', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>eG', builtin.live_grep, {})
-    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+    vim.keymap.set('n', '<leader>ep', builtin.git_files, {})
     vim.keymap.set('n', '<leader>eh', builtin.help_tags, {})
+    vim.keymap.set('n', '<leader>eG', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>eg', function()
         builtin.grep_string({ search = vim.fn.input("Grep > ") });
     end)
