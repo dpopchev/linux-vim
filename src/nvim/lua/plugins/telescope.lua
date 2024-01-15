84cdf2b4da7075c5a373bf3304485b77d95acccc
Dimitar Popchev <dpopchev@gmail.com>
2024-01-15 16:32:41 +0200

    refactor

--- src/nvim/lua/plugins/telescope.lua
+++ src/nvim/lua/plugins/telescope.lua
@@ -1,14 +1,15 @@
 local function setup()
     local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>ee', builtin.builtin, {})
     vim.keymap.set('n', '<leader>ef', builtin.find_files, {})
     vim.keymap.set('n', '<leader>ep', builtin.git_files, {})
-    vim.keymap.set('n', '<leader>eh', builtin.help_tags, {})
+    vim.keymap.set('n', '<leader>et', builtin.help_tags, {})
     vim.keymap.set('n', '<leader>eG', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>eg', function()
         builtin.grep_string({ search = vim.fn.input("Grep > ") });
     end)
     local actions = require "telescope.actions"
-    local mappings = { i = { ["<C-e>"] = actions.close, } }
+    local mappings = { i = { ["<leader>eq"] = actions.close, } }
     local conf = { defaults = { mappings = mappings } }
 
     require('telescope').setup(conf)
